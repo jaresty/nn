5664e97babddbda599f5a61abdcc5212a8f91bf6
Joshua Aresty <jaresty@gmail.com>
2026-04-17 07:03:42 -0700

    fix: show version from VCS build info when installed via go install

--- cmd/nn/main.go
+++ cmd/nn/main.go
@@ -1,29 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/jaresty/nn/cmd/nn/cmd"
 )
 
 // Injected at build time by GoReleaser ldflags.
 var (
-	version = "dev"
-	commit  = "none"
-	date    = "unknown"
+	version = ""
+	commit  = ""
+	date    = ""
 )
 
 func main() {
 	root := cmd.NewRootCmd("")
-	root.Version = version + " (" + commit[:min(7, len(commit))] + " " + date + ")"
+	root.Version = buildVersion()
 	if err := root.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
+// buildVersion returns a version string, preferring GoReleaser ldflags and
+// falling back to VCS info embedded by go install / go build.
+func buildVersion() string {
+	if version != "" {
+		c := commit
+		if len(c) > 7 {
+			c = c[:7]
+		}
+		return fmt.Sprintf("%s (%s %s)", version, c, date)
 	}
-	return b
+
+	if info, ok := debug.ReadBuildInfo(); ok {
+		v := info.Main.Version
+		if v == "" || v == "(devel)" {
+			v = "dev"
+		}
+		var vcsRev, vcsTime string
+		for _, s := range info.Settings {
+			switch s.Key {
+			case "vcs.revision":
+				if len(s.Value) > 7 {
+					vcsRev = s.Value[:7]
+				} else {
+					vcsRev = s.Value
+				}
+			case "vcs.time":
+				vcsTime = s.Value
+			}
+		}
+		if vcsRev != "" {
+			return fmt.Sprintf("%s (%s %s)", v, vcsRev, vcsTime)
+		}
+		return v
+	}
+
+	return "dev"
 }
